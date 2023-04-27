2013c3848cbd307f7e3f5c7b56aa5a8f2c4c4863
jerryraynelson3 <jerryraynelson3@hotmail.com>
2023-04-26 18:07:32 -0600

    FInished the prep4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int number = -2;
+        int sum = 0;
+        
+        Console.WriteLine("Please enter a list of numbers one at a time and type 0 when finished.");
+
+        while(number != 0)
+        {
+
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            numbers.Add(number);
+
+        }
+
+        int numberOfNumbers = numbers.Count - 1;
+
+        for(int i = 0; i < numberOfNumbers; i++)
+        {
+
+            sum += numbers[i];
+
+        }
+
+        float average = sum / numberOfNumbers;
+
+        Console.WriteLine($"The sum of all the numbers is {sum}.");
+        Console.WriteLine($"The Average of all the numbers is {average}.");
+
     }
 }
